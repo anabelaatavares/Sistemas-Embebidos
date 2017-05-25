1d59d599e6daed48759e2c333d4783e278c26eec
ruimigueltrigo <ruimigueltrigo@gmail.com>
2017-05-25 20:22:10 +0100

    blackout + abreLatas + equalizer

--- ComunicacaoSerial/ComunicacaoSerial.ino
+++ ComunicacaoSerial/ComunicacaoSerial.ino
@@ -19,24 +19,147 @@
  This example code is in the public domain.
 
  */
+byte inicio = 32;
+int modo = 0;
+long tempoAtual, tempoInicio;
+int botaoGraves = 13;//cabo branco
+int botaoMedios = 12;//cabo laranja
+int botaoAgudos = 11;//cabo roxo
+int estadoBotaoGraves, estadoBotaoMedios, estadoBotaoAgudos;
+int ledVerde1 = 8;
+int ledVerde2 = 7;
+int ledVerde3 = 6;
+int ledAmarelo1 = 5;
+int ledAmarelo2 = 4;
+int ledAmarelo3 = 3;
+int ledVermelho1 = 2;
+int ledVermelho2 = 9;
+int valorRecebido;
+
 
 
 void setup() {
   // initialize both serial ports:
   Serial.begin(9600);
+  
   Serial1.begin(9600);
+  pinMode(botaoGraves, INPUT);
+  pinMode(botaoMedios, INPUT);
+  pinMode(botaoAgudos, INPUT);
+
+  pinMode(ledVerde1, OUTPUT);
+  pinMode(ledVerde2, OUTPUT);
+  pinMode(ledVerde3, OUTPUT);
+  pinMode(ledAmarelo1, OUTPUT);
+  pinMode(ledAmarelo2, OUTPUT);
+  pinMode(ledAmarelo3, OUTPUT);
+  pinMode(ledVermelho1, OUTPUT);
+  pinMode(ledVermelho2, OUTPUT);
 }
 
 void loop() {
-  // read from port 1, send to port 0:
+//   read from port 1, send to port 0:
   if (Serial1.available()) {
-    int inByte = Serial1.read();
-    Serial.write(inByte);
+    int mByte = Serial1.read();
+    abreLatas(mByte);
   }
+//
+//  // read from port 0, send to port 1:
+//  if (Serial.available()) {
+//    int inByte = Serial.read();
+//    Serial1.write(inByte);
+//  }
 
-  // read from port 0, send to port 1:
-  if (Serial.available()) {
-    int inByte = Serial.read();
-    Serial1.write(inByte);
+  tempoAtual = millis();
+
+  if (tempoAtual - tempoInicio > 1000) {
+    tempoInicio = millis();   
+    estadoBotaoGraves = digitalRead(botaoGraves);
+    estadoBotaoMedios = digitalRead(botaoMedios);
+    estadoBotaoAgudos = digitalRead(botaoAgudos);    
+    
+    //se pressionado (HIGH)
+    if( estadoBotaoGraves == HIGH) {      
+      modo = 10;
+    } else if ( estadoBotaoMedios == HIGH) {
+      modo = 20;
+    } else if ( estadoBotaoAgudos == HIGH) {
+      modo = 30;
+    } else {
+      modo = 0;
+      blackout();
+      equalizer(valorRecebido);
+    }
+    Serial1.write(inicio);
+    Serial1.write((char*) &modo, 2);
   }
 }
+
+void acenderLed(int varLed) {
+  digitalWrite(varLed, HIGH);
+}
+
+void apagarLed(int varLed) {
+  digitalWrite(varLed, LOW);
+}
+
+void abreLatas(int mByte) {
+  byte bytesRecebidos[20];
+  int i = 0;
+  
+  Serial.println(mByte);
+  if(mByte != 32) {
+    bytesRecebidos[i] = mByte;
+    i++;
+  } else {      
+    valorRecebido = bytesRecebidos[i];
+    Serial.println(valorRecebido);
+    bytesRecebidos[i] = 0;
+    i = 0;      
+    //podeEnviar = true;
+  }  
+  
+}
+
+void blackout() {
+  apagarLed(ledVerde1);
+  apagarLed(ledVerde2);
+  apagarLed(ledVerde3);
+  apagarLed(ledAmarelo1);
+  apagarLed(ledAmarelo2);
+  apagarLed(ledAmarelo3);
+  apagarLed(ledVermelho1);
+  apagarLed(ledVermelho2);
+}
+
+void equalizer(int vr) {
+  //todo ajustar
+int  limiteGraves = 50;
+int  limiteMedios = 100;
+
+  if(vr >= 0) {
+    acenderLed(ledVerde1);
+    if(vr > limiteGraves / 3) {
+      acenderLed(ledVerde2);
+      if(vr > (limiteGraves / 3) * 2) {
+        acenderLed(ledVerde3);
+        if(vr > limiteGraves) {
+          acenderLed(ledAmarelo1);
+          if(vr > limiteMedios / 3) {
+            acenderLed(ledAmarelo2);
+            if(vr > (limiteMedios / 3) * 2) {
+              acenderLed(ledAmarelo3);
+              if(vr > limiteMedios) {
+                acenderLed(ledVermelho1);
+                if(vr > limiteMedios * 2) {
+                  acenderLed(ledVermelho2);
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
